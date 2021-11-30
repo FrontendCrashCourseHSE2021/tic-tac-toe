06b693a6d741afcd92efb58dcbaa9c33b5d04c09
Liza <WingAsh29590>
2021-11-30 14:42:30 +0300

    Fix game hw

--- src/Game.tsx
+++ src/Game.tsx
@@ -43,6 +43,23 @@ export function Game() {
      * или null -- пока нет победителя (или уже не будет, если это ничья).
      */
     function calculateWinner(squares: SquareValue[]): SquareValue {
+        const possibles = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < possibles.length; i++) {
+            const [a, b, c] = possibles[i];
+            if (squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+
         // return PLAYER2;
         // return PLAYER1;
         return null;
@@ -54,8 +71,12 @@ export function Game() {
      * @return true если ничья, false если не ничья.
      */
     function isTie(squares: SquareValue[]): boolean {
-        // return true;
-        return false;
+        for (let i = 0; i < squares.length; i++) {
+            if (squares[i] == null){
+                return false;
+            }
+         }
+        return true;
     }
 
     /**
