2a46d5edbcc815d9c3e8dc433aec8aaf7862bc39
savinovava <savinovavaleriya@ya.ru>
2021-12-07 00:36:05 +0300

    '===' instead of '=='

--- src/Game.tsx
+++ src/Game.tsx
@@ -43,16 +43,16 @@ export function Game() {
      * или null -- пока нет победителя (или уже не будет, если это ничья).
      */
     function calculateWinner(squares: SquareValue[]): SquareValue {
-        if (((squares[0]==squares[1])&&(squares[1]==squares[2])&&(squares[0] != null))||
-            ((squares[0]==squares[3])&&(squares[3]==squares[6])&&(squares[6] != null))||
-            ((squares[0]==squares[4])&&(squares[4]==squares[8])&&(squares[0] != null)))
+        if (((squares[1] === squares[0])&&(squares[1]===squares[2])&&(squares[0] != null))||
+            ((squares[0]===squares[3])&&(squares[3]===squares[6])&&(squares[6] != null))||
+            ((squares[0]===squares[4])&&(squares[4]===squares[8])&&(squares[0] != null)))
         {return squares[0]}
-        else if (((squares[3]==squares[4])&&(squares[4]==squares[5])&&(squares[3] != null))||
-        ((squares[1]==squares[4])&&(squares[4]==squares[7])&&(squares[4] != null))||
-            ((squares[2]==squares[4])&&(squares[4]==squares[6])&&(squares[4] != null)))
+        else if (((squares[3]===squares[4])&&(squares[4]===squares[5])&&(squares[3] != null))||
+        ((squares[1]===squares[4])&&(squares[4]===squares[7])&&(squares[4] != null))||
+            ((squares[2]===squares[4])&&(squares[4]===squares[6])&&(squares[4] != null)))
         {return squares[4]}
-        else if (((squares[2]==squares[5])&&(squares[5]==squares[8])&&(squares[8] != null))||
-        ((squares[6]==squares[7])&&(squares[7]==squares[8])&&(squares[6] != null))){return squares[8]}
+        else if (((squares[2]===squares[5])&&(squares[5]===squares[8])&&(squares[8] != null))||
+        ((squares[6]===squares[7])&&(squares[7]===squares[8])&&(squares[6] != null))){return squares[8]}
         // return PLAYER2;
         // return PLAYER1;
         else return null;
