97dae0be0d9ba587a1bf40d58b4e8a80ee628fa5
savinovava <savinovavaleriya@ya.ru>
2021-12-07 00:11:22 +0300

    fix tests

--- src/Game.tsx
+++ src/Game.tsx
@@ -32,7 +32,7 @@ interface GameState {
 export function Game() {
     // получаем состояние игры и функцию для его изменения
     const [state, changeState] = useState<GameState>({
-        squares: Array(9).fill(null) as Array<SquareValue>,
+        squares: Array(9).fill(null),
         xIsNext: true
     });
 
@@ -43,11 +43,20 @@ export function Game() {
      * или null -- пока нет победителя (или уже не будет, если это ничья).
      */
     function calculateWinner(squares: SquareValue[]): SquareValue {
+        if (((squares[0]==squares[1])&&(squares[1]==squares[2])&&(squares[0] != null))||
+            ((squares[0]==squares[3])&&(squares[3]==squares[6])&&(squares[6] != null))||
+            ((squares[0]==squares[4])&&(squares[4]==squares[8])&&(squares[0] != null)))
+        {return squares[0]}
+        else if (((squares[3]==squares[4])&&(squares[4]==squares[5])&&(squares[3] != null))||
+        ((squares[1]==squares[4])&&(squares[4]==squares[7])&&(squares[4] != null))||
+            ((squares[2]==squares[4])&&(squares[4]==squares[6])&&(squares[4] != null)))
+        {return squares[4]}
+        else if (((squares[2]==squares[5])&&(squares[5]==squares[8])&&(squares[8] != null))||
+        ((squares[6]==squares[7])&&(squares[7]==squares[8])&&(squares[6] != null))){return squares[8]}
         // return PLAYER2;
         // return PLAYER1;
-        return null;
+        else return null;
     }
-
     /**
      * Вычисляет ничья в игре или нет.
      * @param squares Массив игрового поля.
@@ -55,7 +64,10 @@ export function Game() {
      */
     function isTie(squares: SquareValue[]): boolean {
         // return true;
-        return false;
+        if (!(calculateWinner(squares))&&!(squares.includes(null))){
+            return true
+        }
+        else return false;
     }
 
     /**
